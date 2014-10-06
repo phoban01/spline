d535544adf9376acaf089f1ad234ae80e16313b9
phoban01 <phoban01@gmail.com>
2014-10-06 18:49:54 +0100

    Initial Commit

---
+++ splines/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  splines
+//
+//  Created by Piaras Hoban on 06/10/2014.
+//  Copyright (c) 2014 Piaras Hoban. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
